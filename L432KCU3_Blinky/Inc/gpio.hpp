1c5f4b4bfb35cc0e7b4f7a49e40075c01ed9a3c9
cracked-machine <chrisjsutton@gmail.com>
2022-05-14 21:51:39 +0100

    updated comments

--- L432KCU3_Blinky/Inc/gpio.hpp
+++ L432KCU3_Blinky/Inc/gpio.hpp
@@ -63,7 +63,13 @@ enum class Pupd
 class Driver
 {
 public:
-
+    /// @brief Construct a new gpio driver
+    /// @param gpio_port The gpio port (GPIOA, GPIOB, GPIOC, etc..)
+    /// @param gpio_pin The gpio pin (PIN0...PIN15)
+    /// @param mode The gpio mode (output, input, analog, alternate function)
+    /// @param otype The gpio type (pushpull/opendrain)
+    /// @param speed The gpio speed (low/medium/high/very high)
+    /// @param pupd The gpio pullup/pulldown setting
     Driver( GPIO_TypeDef* gpio_port, 
             Pin gpio_pin,
             Mode mode,
@@ -71,19 +77,38 @@ public:
             Speed speed,
             Pupd pupd);        
 
+    /// @brief Set the mode
+    /// @param mode The gpio mode (output, input, analog, alternate function)
     void set_mode(Mode mode);
+
+    /// @brief Set the type
+    /// @param otype The gpio type (pushpull/opendrain)
     void set_otype(Otype otype);
+
+    /// @brief Set the speed
+    /// @param speed The gpio speed (low/medium/high/very high)
     void set_speed(Speed speed);
+
+    /// @brief Set the pupd
+    /// @param pupd The gpio pullup/pulldown setting
     void set_pupd(Pupd pupd);
+
+    /// @brief Toggle the pin value
     void toggle();
+
+    /// @brief Set the pin to 1
     void set_pin();
+
+    /// @brief Reset the pin 0
     void reset_pin();
 
 private:
+    /// @brief The GPIO register object (port)
     GPIO_TypeDef* m_gpio_port;
+    /// @brief The GPIO pin object
     Pin m_gpio_pin;
+    /// @brief Convenience member; 0x1UL bitwise-shifted right by pin number
     uint16_t _pin_shifted = 0x1UL << static_cast<uint16_t>(m_gpio_pin);
-
 };
 
 } // namespace stm32
